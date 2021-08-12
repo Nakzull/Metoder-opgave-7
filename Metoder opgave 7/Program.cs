d382b18984db4e4bcabac47b6c4eec8c77424f2e
Nakzull <andr25f4@zbc.dk>
2021-08-12 11:49:39 +0200

    added the other parts of the assignment

--- Metoder opgave 7/Program.cs
+++ Metoder opgave 7/Program.cs
@@ -8,14 +8,14 @@ namespace Metoder_opgave_7
         {
             int[] myArray = new int[9];
             InsertNumbersToArray(ref myArray);
-
-            foreach (int items in myArray)
-                Console.WriteLine(items);
+            for (int i = 4; i < 5; i++)
+                Console.WriteLine("The fifth value of the array is " + myArray[i]);
+            myArray[4] = myArray[3] * 2;
         }
         public static int[] InsertNumbersToArray(ref int[] a)
         {
             for (int i = 0; i < 9; i++)
-               a[i] = i + 1;
+                a[i] = i + 1;
             return a;
         }
     }

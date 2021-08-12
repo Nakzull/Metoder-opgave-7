1e00aa9b515e046dc1a816d68a7ee8e2a39811d8
zbcabla <zbcabla@ZBC-E-CH-SKP047>
2021-08-12 11:38:33 +0200

    Add project files.

---
+++ Metoder opgave 7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Metoder_opgave_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[9];
+            InsertNumbersToArray(ref myArray);
+
+            foreach (int items in myArray)
+                Console.WriteLine(items);
+        }
+        public static int[] InsertNumbersToArray(ref int[] a)
+        {
+            for (int i = 0; i < 9; i++)
+               a[i] = i + 1;
+            return a;
+        }
+    }
+}
